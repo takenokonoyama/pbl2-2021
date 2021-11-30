a811af72b8ec560c30fd502f4afb6f29892e9a53
yosiking0330 <niimura.yoshiki.19@shizuoka.ac.jp>
2021-11-30 23:22:24 +0900

    クライアント

--- client.py
+++ client.py
@@ -14,34 +14,6 @@ server_file_name = sys.argv[4] # ファイル名
 token_str = sys.argv[5] # トークン文字列
 rec_file_name = 'received_data.dat' # 受け取ったデータを書き込むファイル
 
-# SIZE要求
-def SIZE_req(soc, file_name):
-    msg = f'SIZE {file_name}\n' # 要求メッセージ
-    soc.send(msg.encode())
-    print('request SIZE')
-
-# GET要求(ALL)
-def GET_req_all(soc, file_name,token_str):
-    key = pbl2.genkey(token_str)
-    msg = f'GET {file_name} {key} ALL\n' # 要求メッセージ
-    soc.send(msg.encode())
-    print('request GET ALL')
-
-# GET要求(PARTIAL)
-def GET_req_part(soc,file_name,token_str,sB, eB):
-    key = pbl2.genkey(token_str) # keyの作成
-    msg = f'GET {file_name} {key} PARTIAL {sB} {eB}\n' # 要求メッセージ
-    soc.send(msg.encode())
-    print('request GET PARTIAL')
-
-# REP要求
-def REP_req(soc, file_name, token_str):
-    key = pbl2.genkey(token_str) # keyの作成
-    repkey_out = pbl2.repkey(key, rec_file_name) # repkeyの作成
-    msg = f'REP {file_name} {repkey_out}\n' # 要求メッセージ
-    soc.send(msg.encode())
-    print('request REP')
-
 # 応答の受け取り
 def rec_res(soc):
     # 応答コードの受け取り
@@ -54,6 +26,56 @@ def rec_res(soc):
         recv_bytearray.append(b)
     print('received response')
     print(rec_str)
+
+# SIZE 
+def SIZE(soc, file_name):
+    # 要求
+    msg = f'SIZE {file_name}\n' # 要求メッセージ
+    soc.send(msg.encode())
+    print('request SIZE')
+    
+    # 応答の受け取り
+    rec_res(soc)
+    soc.close()
+
+# GET(ALL)
+def GET_all(soc, file_name,token_str):
+    # 要求
+    key = pbl2.genkey(token_str)
+    msg = f'GET {file_name} {key} ALL\n' # 要求メッセージ
+    soc.send(msg.encode())
+    print('request GET ALL')
+    
+    # 応答の受け取り
+    rec_res(soc)
+    receive_server_file(soc)
+    soc.close()
+
+
+# GET(PARTIAL)
+def GET_part(soc,file_name,token_str,sB, eB):
+    # 要求
+    key = pbl2.genkey(token_str) # keyの作成
+    msg = f'GET {file_name} {key} PARTIAL {sB} {eB}\n' # 要求メッセージ
+    soc.send(msg.encode())
+    print('request GET PARTIAL')
+
+    # 応答の受け取り
+    rec_res(soc)
+    receive_server_file(soc)
+    soc.close()    
+
+# REP
+def REP(soc, file_name, token_str):
+    key = pbl2.genkey(token_str) # keyの作成
+    repkey_out = pbl2.repkey(key, rec_file_name) # repkeyの作成
+    msg = f'REP {file_name} {repkey_out}\n' # 要求メッセージ
+    soc.send(msg.encode())
+    print('request REP')
+    
+    # 応答の受け取り
+    rec_res(soc)
+    soc.close()
 
 # serverからのfileの受け取り
 def receive_server_file(soc):
@@ -76,36 +98,26 @@ if __name__ == '__main__':
     # SIZE 
     client_socket = socket(AF_INET, SOCK_STREAM)  # ソケットを作る
     client_socket.connect((server_name, server_port))  # サーバのソケットに接続する
-    SIZE_req(client_socket, server_file_name) # SIZE要求
-    rec_res(client_socket) # 応答受け取り
-    client_socket.close() # ソケットを閉じる
-    
+    SIZE(client_socket, server_file_name) # SIZEコマンド
+
     print()
 
     # GET(ALL)
     # 要求を2つ以上行う場合、ソケットをもう一度作る必要がある
     client_socket = socket(AF_INET, SOCK_STREAM)  # ソケットを作る
     client_socket.connect((server_name, server_port))  # サーバのソケットに接続する
-    GET_req_all(client_socket, server_file_name, token_str) # GET(ALL)要求
-    rec_res(client_socket) # 応答受け取り
-    receive_server_file(client_socket) # ファイルダウンロード
-    client_socket.close() # ソケットを閉じる
+    GET_all(client_socket, server_file_name, token_str) # GET(ALL)コマンド
 
     print()
 
     # GET(PARTIAL)
     # client_socket = socket(AF_INET, SOCK_STREAM)  # ソケットを作る
     # client_socket.connect((server_name, server_port))  # サーバのソケットに接続する
-    # GET_req_part(client_socket, server_file_name, token_str, 0, 10) # GET(PARTIAL)要求
-    # rec_res(client_socket) # 応答受け取り
-    # receive_server_file(client_socket) # ファイルダウンロード
-    # client_socket.close()
+    # GET_part(client_socket, server_file_name, token_str, 0, 10) # GET(PARTIAL)コマンド
 
     print()
 
     # REP
     client_socket = socket(AF_INET, SOCK_STREAM)  # ソケットを作る
     client_socket.connect((server_name, server_port))  # サーバのソケットに接続する
-    REP_req(client_socket, server_file_name, token_str) # REP要求
-    rec_res(client_socket) # 応答受け取り
-    client_socket.close() # ソケットを閉じる
+    REP(client_socket, server_file_name, token_str) # REPコマンド
