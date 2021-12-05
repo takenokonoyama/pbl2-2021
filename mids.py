e60c86a8a2526f199a53be8745d4f99017bc5961
takesGit <takezawa.hiroki.19>
2021-12-05 11:01:32 +0900

    GETコマンドも中間サーバいけます

--- mids.py
+++ mids.py
@@ -4,15 +4,16 @@
 from socket import *
 import sys
 import threading  # for Thread()
+import os
 
 BUFSIZE = 1024 # 受け取る最大のファイルサイズ
-rec_file_name = 'received_data.dat' # 受け取ったデータを書き込むファイル
+rec_file_name = 'midreceived_data.dat' # 受け取ったデータを書き込むファイル
 
 mid_name = sys.argv[1]  # 中間サーバのホスト名あるいはIPアドレスを表す文字列
 server_name = sys.argv[2] # サーバのホスト名
 server_port =  int(sys.argv[3]) # サーバのポート
 
-mid_port = 53009
+mid_port = 53013
 
 def rec_res(soc):
     # 応答コードの受け取り
@@ -27,29 +28,61 @@ def rec_res(soc):
     print('received response')
     return rec_str
 
-def mid_server(server_name, server_port,sentence):#中間サーバとサーバの通信
+def receive_server_file(soc):
+    # 書き込み用ファイルをオープンして処理
+    #   ファイル絡みの例外処理とクローズの処理は書く必要がありません
+    with open(rec_file_name, 'wb') as f: # 'wb' は「バイナリファイルを書き込みモードで」という意味
+        while True:
+            data = soc.recv(BUFSIZE)   # BUFSIZEバイトずつ受信
+            if len(data) <= 0:  # 受信したデータがゼロなら、相手からの送信は全て終了
+                break
+            f.write(data)  # 受け取ったデータをファイルに書き込む
+
+def mid_server(server_name, server_port,sentence,com):#中間サーバとサーバの通信
     mid_socket = socket(AF_INET, SOCK_STREAM)  # ソケットを作る
     mid_socket.connect((server_name, server_port))#中間、サーバとのコネクト
     print('Sending to server: {0}'.format(sentence))
     print(server_name)
     print(server_port)
 
-    mid_socket.send(sentence.encode())
-
+    mid_socket.send(sentence.encode())  
     rep = rec_res(mid_socket)
     print(rep)
+    print(com)
+
+    if com =="GET" :
+        receive_server_file(mid_socket)
+        return rep
+
     mid_socket.close()
     return rep
 
 def interact_with_client_TCP(soc):
     sentence = rec_res(soc)
     print('Received: {0}'.format(sentence)) 
-    rep_sentence=mid_server(server_name, server_port,sentence)
+    print(sentence[0:3])
+    com=sentence[0:3] 
 
+    rep_sentence=mid_server(server_name, server_port,sentence,com)
     print('Sending to client: {0}'.format(rep_sentence))
     soc.send(rep_sentence.encode())
+
+    if com=="GET":
+        #"midreceived_data.dat"を送りたい
+        #現状ファイルの中身を一度開いて一文字ずつ送ってる
+        openfile("midreceived_data.dat",soc)
+
     print("Finish Sending")
     soc.close()
+
+def openfile(file_name,soc) :
+    path=os.getcwd()
+    print(path)
+    path +="/"
+    path +=file_name
+    with open(path) as f:
+        s = f.read()
+        soc.send(s.encode())
 
 def main_TCP(): #クライアントと中間サーバの通信
     mid_socket = socket(AF_INET, SOCK_STREAM) # ソケットを作る
